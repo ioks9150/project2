dc596b0a139b46c7a11ab8e46997a4aac9ab25f5
ioks9150 <ioks9150@naver.com>
2020-10-19 15:57:45 +0900

    sdfsf

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,7 @@ $(function () {
             $('#header.on .search a img').attr('src', 'images/search2.png');
             $('#header.on .menu a img').attr('src', 'images/menu2.png');
         }
-        if (scr === 0 && width_size>768) {
+        if (scr === 0 && width_size>770) {
             $('#header').removeClass('on');
             $('#header .logo a img').attr('src', 'images/logo.png');
             $('#header .search a img').attr('src', 'images/search.png');
